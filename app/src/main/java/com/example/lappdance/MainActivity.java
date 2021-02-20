a5b82fa2887a6f52119bd994480dd2f08e9636af
FlorianEpsi <73948575+FlorianEpsi@users.noreply.github.com>
2021-02-21 00:02:54 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/lappdance/MainActivity.java
+++ app/src/main/java/com/example/lappdance/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.lappdance;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.play);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    GridView();
+            }
+        });
     }
 }
