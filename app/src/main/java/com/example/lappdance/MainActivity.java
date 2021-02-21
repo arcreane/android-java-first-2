95d11f6826a3927ac21be1be4f6238e97c579688
taoufik <taourirt@live.fr>
2021-02-21 02:04:20 +0100

    adding activity to change view

--- app/src/main/java/com/example/lappdance/MainActivity.java
+++ app/src/main/java/com/example/lappdance/MainActivity.java
@@ -1,27 +1,21 @@
 package com.example.lappdance;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        button = (Button) findViewById(R.id.play);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                    GridView();
-            }
-        });
+        }
+    public void sudoku(View view) {
+        Intent intent = new Intent(MainActivity.this, Sudoku.class);
+        startActivity(intent);
     }
+
 }
---
+++ app/src/main/java/com/example/lappdance/Sudoku.java
@@ -0,0 +1,14 @@
+package com.example.lappdance;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Sudoku extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sudoku);
+    }
+}
